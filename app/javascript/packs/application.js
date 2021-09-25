19c996c9d6134d06626ce8f651b6884b820c607b
Yukio Arie <yukioarie@gmail.com>
2021-09-24 22:24:23 -0300

    import dependencies libs to user_backoffice

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,22 @@ import Rails from "@rails/ujs"
 
 // import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-//import 'jquery';
-require('stylesheets/sb-admin-2.min.css');
-require('jquery')
 import "channels"
+//import css
+require('stylesheets/sb-admin-2.css');
+require('stylesheets/custom.css');
+//import scripts
+require('jquery');
+require("src/sb-admin-2.js");
+require("src/custom.js");
+//import libs using yarn
 import 'bootstrap/dist/js/bootstrap'
 import 'jquery.easing/jquery.easing.min.js'
-require("src/sb-admin-2.min.js")
+import 'font-awesome/css/font-awesome.css'
+import 'nprogress/nprogress.js'
+import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js'
+import 'fastclick/lib/fastclick.js'
+import 'metismenu/src/index.js'
 
 //global.$ = require('jquery')
 
