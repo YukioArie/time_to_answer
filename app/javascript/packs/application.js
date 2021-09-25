bace4955661fbb4d4f92997553e6b7f23521a330
Yukio Arie <yukioarie@gmail.com>
2021-09-25 00:21:37 -0300

    add image asset

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,11 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//images
+require.context('../images', true)
 
 import Rails from "@rails/ujs"
+
 
 // import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -23,6 +26,7 @@ import 'nprogress/nprogress.js'
 import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js'
 import 'fastclick/lib/fastclick.js'
 import 'metismenu/src/index.js'
+
 
 //global.$ = require('jquery')
 
