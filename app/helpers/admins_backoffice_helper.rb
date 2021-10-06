db427159608d0fddbc3308f636fc1263e4f55136
Yukio Arie <yukioarie@gmail.com>
2021-10-06 02:33:59 -0300

    create helper for use i18n

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,9 @@
 module AdminsBackofficeHelper
+  def translate_atribute(object= nil, method= nil)
+    if object && method 
+      object.model.human_attribute_name(method)
+    else
+      "Informe os campos"
+    end
+  end
 end
