08678dd32efebd7113bd411d53a165f87ce7240d
Yukio Arie <yukioarie@gmail.com>
2021-10-07 02:15:33 -0300

    add layout admin_devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resourcce
+
+  private
+
+  def layout_by_resourcce
+    if devise_controller? && resource_class == Admin
+      'admin_devise'
+    else
+      'application'
+    end
+  end
 end
