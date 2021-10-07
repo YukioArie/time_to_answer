fcf68f09d9f51867f6f958ea2d26ce0a25272a69
Yukio Arie <yukioarie@gmail.com>
2021-10-06 22:07:25 -0300

    correct model name answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :subject
-  has_many :anwers
+  has_many :answers
   accepts_nested_attributes_for :answers, allow_destroy: true
   paginates_per 5
 end
