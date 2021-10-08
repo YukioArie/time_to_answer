84bb51df70482ca91be4d71c5c49b9fa155952a3
Yukio Arie <yukioarie@gmail.com>
2021-10-07 22:53:38 -0300

    refactor layout_by_resource function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
-  layout :layout_by_resourcce
+  layout :layout_by_resource
 
   private
 
-  def layout_by_resourcce
-    if devise_controller? && resource_class == Admin
-      'admin_devise'
-    else
-      'application'
-    end
+  def layout_by_resource
+    devise_controller? ? "#{resource_class.to_s.downcase}_devise" : 'application'
   end
 end
